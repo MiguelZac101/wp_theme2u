36faafeb59a074cecb6b81179309cbf7bd6a7038
Miguel Quispe <miguel.quispe@fractal.com.pe>
2018-06-11 17:11:29 -0500

    181. Editando el CSS de nuestro menú.

--- wp-content/themes/theme2u/header.php
+++ wp-content/themes/theme2u/header.php
@@ -14,14 +14,17 @@
             <header id="masthead" class="site-header" role="banner">
                 <div class="container">
                     
-                    <a href="<?php echo esc_url(home_url('/'));?>">
-                        <?php // bloginfo('name'); ?>
-                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo.svg"/>
-                    </a>
+                    <div class="logo">
+                        <a href="<?php echo esc_url(home_url('/'));?>">
+                            <?php // bloginfo('name'); ?>
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo.svg"/>
+                        </a>
+                    </div>                    
+                    
                     <nav id="site-navigation" class="main-navigation" role="navigation">
                         <?php wp_nav_menu(array('theme_location' => 'menu_principal')); ?>
                     </nav>
-                    
+                    <div class="clear"></div>
 
                 </div>
             </header>
