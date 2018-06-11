a52a2d1f51c1815c512ebacd404b0686b62f2d63
Miguel Quispe <miguel.quispe@fractal.com.pe>
2018-06-11 17:40:55 -0500

    182. Creando el Menú en el Footer.

--- wp-content/themes/theme2u/footer.php
+++ wp-content/themes/theme2u/footer.php
@@ -1,6 +1,14 @@
     </div> <!--#contenido-->
     <footer class="site-footer" role="contentinfo">
-        desde el footer
+        <div class="container">
+            <nav id="footer-navigaion" class="footer-navigation">
+                <?php wp_nav_menu(array('theme_location'=>'menu_principal'));?>
+            </nav>
+        </div><!--container-->
+        <div class="clear"></div>
+        <div class="copyright">
+            <p>Visita Toronto <?php echo date('Y'); ?></p>
+        </div>
     </footer>
 </div> <!--#page-->
 </body>
