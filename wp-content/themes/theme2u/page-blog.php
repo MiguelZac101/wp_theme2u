d65cafe85cb24ce1173cae38474903e3b1b27f42
Miguel Quispe <miguel.quispe@fractal.com.pe>
2018-06-13 12:30:25 -0500

    189. Creando una página avanzada para nuestro Blog. (PLANTILLA)

---
+++ wp-content/themes/theme2u/page-blog.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * Template Name: Guía Toronto 
+ */
+?>
+<?php get_header(); ?>
+
+<?php while (have_posts()): the_post(); ?>
+
+    <?php if (has_post_thumbnail()) { ?>   
+        <div class="destacada">
+            <?php the_post_thumbnail('destacada'); ?>
+            <h2>
+                <?php the_title(); ?>
+            </h2>        
+        </div>
+    <?php } else { ?>
+        <h2 class="noimagen"><?php the_title(); ?></h2>
+    <?php } ?>
+
+    <div id="primary" class="primary">
+
+        desde page-blog.php
+
+    </div>
+<?php endwhile; ?>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
